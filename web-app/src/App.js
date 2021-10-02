bea3559da7a360ab4ba9a0777cfe79fee7d4af00
DiboraHaile <deborahaile3@gmail.com>
2021-10-02 17:21:03 +0300

    send data to backend

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component, useEffect ,useState} from 'react'
-
+import { Switch, Route } from 'react-router-dom'
 import Select from 'react-select'
 import useFetch from "react-fetch-hook"
 
@@ -17,6 +17,7 @@ class App extends Component {
     this.state = {
                   data:{"ids":["hello"]},
                   selected_id: "",
+                  top_sites_info: {},
                   };
     this.handleSelect = this.handleSelect.bind(this);
     this.selected_id = null
@@ -46,26 +47,34 @@ class App extends Component {
   })
   .then(response => response.json())
   .catch(error => console.log(error))
-  // alert(e.target.value)
+ 
+    async function fetching_data() {
+      const response = await fetch("http://localhost:5000/");
+      const data = await response.json();
+      return data;
+    }
+    fetching_data().then(data => {
+      this.setState({
+        top_sites_info: data["top_sites"]
+      })
+    });
   }
+  
     
 
   
   render(){
     const data = this.state.data
+    const top_sites = this.state.top_sites_info
     const prepare_options =  function (){
     const options = []
       for (let i = 1; i <= data['ids'].length; i++) {
-        options.push(<option key={data['ids'][i]} value={data['ids'][i]}>{data['ids'][i]}</option>);   
-        // var option = {}
-        // option['value'] = data['ids'][i];
-        // option['label'] = data['ids'][i];
-        // options.push(option)
-        
+        options.push(<option key={data['ids'][i]} value={data['ids'][i]}>{data['ids'][i]}</option>);      
       }
-
       return options; 
     }
+
+    
     
     
   
@@ -74,11 +83,12 @@ class App extends Component {
     <div className="App">
       <h2> Select Campaign from the following:</h2>
       <div>
-       
-       {/* <Select onSelect={this.handleSelect} options={prepare_options()}/>  */}
       <select name ="SelectCaptionId" id ="SelectCaptionId" onChange={this.handleSelect}>
         {prepare_options()}
       </select>
+      <div>
+        {this.state.top_sites}
+        </div>
       </div>
       
     
